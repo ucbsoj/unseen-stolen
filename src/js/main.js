c129186127666dfa49f6c676ecffdfa123688149
Jeremy Rue <jrue@users.noreply.github.com>
2019-04-23 22:51:43 -0700

    Add trump effect-like design, first draft

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,25 @@
 import $ from "jquery"; // importing jQuery, you can delete if not needed
 require("./lib/social"); // Twitter intent JS
+var skrollr = require("skrollr");
+
+//var s = skrollr.init();
+setTimeout(function(){
+    var s = skrollr.init({forceHeight: false});
+}, 100);
 
 
+//var imgReveal = $('#top-quote').offset().top;
 
-var imgReveal = $('#top-quote').offset().top;
+
+$(document).ready(function(){
+
+	$(".left-side, .top-side, .bottom-side, .right-side, .line-down-middle, #background-line-down-middle").addClass("active");
+
+	$("#background-line-down-middle").one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend", function(){
+		$(this).css("transition", "none");
+	});
+
+});
 
 
 
